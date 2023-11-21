08d89a5083a73f62f27a61e6824b4623a4804e4d
MaryTU <120627741+MaryTU@users.noreply.github.com>
2023-11-21 13:05:39 +0300

    Update main.cpp

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,44 +1,78 @@
 #include "include/my_queue.h"
 
 #include <iostream>
+#include <ctime>
 
 int main()
 {
-    MyQueue<int> md;
-    std::cout<<"is empty: "<<md.isEmpty()<<std::endl;
-    std::cout<<"Size = "<<md.getSize()<<std::endl;
-    std::cout<<"First element = "<<md.getFirst()<<std::endl;
+    std::srand(std::time(NULL));
 
-    std::cout<<"\nadd 5"<<std::endl;
-    md.pushBack(5);
-    std::cout<<"is empty: "<<md.isEmpty()<<std::endl;
-    std::cout<<"Size = "<<md.getSize()<<std::endl;
-    std::cout<<"First element = "<<md.getFirst()<<std::endl;
+    int n = rand()%50 + 10; 
 
-    std::cout<<"\nadd 6"<<std::endl;
-    md.pushBack(6);
-    std::cout<<"is empty: "<<md.isEmpty()<<std::endl;
-    std::cout<<"Size = "<<md.getSize()<<std::endl;
-    std::cout<<"First element = "<<md.getFirst()<<std::endl;
-    
-    // std::cout<<"\ndelete first"<<std::endl;
-    // md.popFirst();
-    // std::cout<<"is empty: "<<md.isEmpty()<<std::endl;
-    // std::cout<<"Size = "<<md.getSize()<<std::endl;
-    // std::cout<<"First element = "<<md.getFirst()<<std::endl;
+    MyQueue<int> queue_int;
 
-    // std::cout<<"\ndelete first"<<std::endl;
-    // md.popFirst();
-    // std::cout<<"is empty: "<<md.isEmpty()<<std::endl;
-    // std::cout<<"Size = "<<md.getSize()<<std::endl;
-    // std::cout<<"First element = "<<md.getFirst()<<std::endl;
+    std::cout<<"is empty: "<<queue_int.isEmpty()<<"\n";
+    //std::cout<<"First element = "<<queue_int.getFirst()<<"\t";
 
+    for (int i = 0; i < n; i++){
+        int rand_value = rand();
 
-    //int a[] = {1,2,3,4,5};
-    for (auto aa:md){
-        std::cout<<aa<<"hey ";
+        std::cout<<"\nadd "<<rand_value<<std::endl;
+        queue_int.pushBack(rand_value);
+
+        std::cout<<"is empty: "<<queue_int.isEmpty()<<"\t";
+        std::cout<<"Size = "<<queue_int.getSize()<<"\t";
+
+        if(queue_int.getSize()){
+            std::cout<<"First element = "<<queue_int.getFirst()<<"\t";
+            if(rand()%2){
+                std::cout<<"delete first element\t";
+                queue_int.popFirst();
+            }
+        }
+        
+        std::cout<<std::endl<<"all element queue: ";
+        for (auto q1:queue_int){
+            std::cout<<q1<<", ";
+        }
+
+        std::cout<<"\n";
     }
-    
+
+
+
+    MyQueue<char> queue_char;
+    char en_symb[] = {"qwertyuiopasdfghjklzxcvbnm"};
+    std::cout<<"is empty: "<<queue_char.isEmpty()<<"\n";
+    for (int i = 0; i < n; i++){
+        int rand_value = rand()%26;
+
+        std::cout<<"\nadd "<<rand_value<<std::endl;
+        queue_char.pushBack(en_symb[rand_value]);
+
+        std::cout<<"is empty: "<<queue_char.isEmpty()<<"\t";
+        std::cout<<"Size = "<<queue_char.getSize()<<"\t";
+
+        if(queue_char.getSize()){
+            std::cout<<"First element = "<<queue_char.getFirst()<<"\t";
+            if(rand()%2){
+                std::cout<<"delete first element\t";
+                queue_char.popFirst();
+            }
+        }
+        
+        std::cout<<std::endl<<"all element queue: ";
+        for (auto q1:queue_char){
+            std::cout<<q1<<", ";
+        }
+
+        std::cout<<"\n";
+    }
+
+    // std::cout<<std::endl<<"all element queue: ";
+    // for (auto q1:queue_int){
+    //     std::cout<<q1<<", ";
+    // }
 
     return 0;
 }
